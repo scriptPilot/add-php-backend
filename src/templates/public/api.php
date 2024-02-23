4106567782dc733ae9e2b854ad2b243d3582591a
scriptPilot <dennis@scheffel.de>
2024-02-23 23:40:42 +0100

    users table authorization
    closes #5

--- src/templates/public/api.php
+++ src/templates/public/api.php
@@ -14,20 +14,51 @@ require('vendor/autoload.php');
 @define('MYSQL_USERNAME', 'root');
 @define('MYSQL_PASSWORD', 'root');
 
+// User ID
+session_start();
+define('USERID', isset($_SESSION['user']['id']) ? $_SESSION['user']['id'] : 0);
+
 // Configuration
 $config = new Config([
 
     // Debug Mode
     'debug' => MYSQL_DATABASE === 'development',
 
-    // Database
+    // Database Credentials
     'address' => MYSQL_HOST,
     'database' => MYSQL_DATABASE,
     'username' => MYSQL_USERNAME,
     'password' => MYSQL_PASSWORD,
 
     // Database Authentication
-    'middlewares' => 'dbAuth'
+    'middlewares' => 'dbAuth,authorization',
+    'dbAuth.mode' => 'optional',
+    'dbAuth.registerUser' => '1',
+    'dbAuth.returnedColumns' => 'id,username',
+    'authorization.tableHandler' => function ($operation, $tableName) {        
+        // Disallow user table for delete operations
+        if ($operation === 'delete' && $tableName === 'users') {
+            return false;
+        }
+        // No other table limitation
+        return true;
+    },
+    'authorization.columnHandler' => function ($operation, $tableName, $columnName) {
+        // Hide user/password column
+        if (($operation === 'read' || $operation === 'list') && $tableName === 'users' && $columnName === 'password') {
+            return false;
+        }
+        // No other column limitation
+        return true;
+    },    
+    'authorization.recordHandler' => function ($operation, $tableName) {
+        // Limit user records to same user
+        if ($tableName === 'users' && $operation !== 'create') {
+            return 'filter=id,eq,' . USERID;
+        }
+        // No other record limitation
+        return true;
+    }    
     
 ]);
 
