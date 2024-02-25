240cddec781d65a9a06920bb24336cc6d463135d
scriptPilot <dennis@scheffel.de>
2024-02-25 21:20:00 +0100

    simplification; capital keywords

--- src/templates/schema.sql
+++ src/templates/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` integer(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` INTEGER(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `username` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS `tasks` (
-  `id` integer(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `done` integer(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` INTEGER(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `done` INTEGER(1) NOT NULL DEFAULT 0
+);
